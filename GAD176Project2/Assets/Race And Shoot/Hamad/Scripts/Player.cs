d2e06ce9361328d360d882dbdc1ff021f2cf1cc2
NullPerson <edu1khoory@hotmail.com>
2024-07-27 18:34:30 +0400

    Fixing Player Prefab

--- GAD176Project2/Assets/Race And Shoot/Hamad/Scripts/Player.cs
+++ GAD176Project2/Assets/Race And Shoot/Hamad/Scripts/Player.cs
@@ -9,16 +9,55 @@ public class Player : MonoBehaviour
     [SerializeField] private int health;
     [SerializeField] private float turnSpeed;
     [SerializeField] private Rigidbody2D _rb2D;
+    [SerializeField] private GameObject[] bullets;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb2D.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (_rb2D == null)
+        {
+            Debug.Log("ERROR: Player Doesn't have access to Rigidbody2D");
+        }
+
+        MovePlayer();
+        Shoot();
+
+
+        if (health <= 0)
+        {
+            DestroyPlayer();
+        }
+
+
     }
+
+    void MovePlayer()
+    {
+
+
+    }
+
+    void Shoot()
+    {
+
+
+    }
+
+    void DestroyPlayer()
+    {
+
+        Destroy(gameObject);
+
+    }
+
+
+
 }
