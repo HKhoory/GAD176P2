64d242c81acc8fdaf2f8268cfac501009a558d98
Leon Colaco <leon.colaco@student.saedubai.com>
2024-08-18 16:41:39 +0400

    Comments for eventmanager

--- GAD176Project2/Assets/Race And Shoot/Leon/Scripts/EventManager.cs
+++ GAD176Project2/Assets/Race And Shoot/Leon/Scripts/EventManager.cs
@@ -62,32 +62,44 @@ public class EventManager : MonoBehaviour
 
     public void Blackhole()
     {
+        //Quad and meshcollider to check the size of the screen, which allows us to find a vector2 to spawn the blackhole on the screen
+        mesh = quad.GetComponent<MeshCollider>();
         screenX = Random.Range(mesh.bounds.min.x, mesh.bounds.max.x);
         screenY = Random.Range(mesh.bounds.min.y, mesh.bounds.max.y);
         position = new Vector2 (screenX, screenY);
 
+        //Spawns the object on the position with the spawnmsg on console
         Instantiate(BlackHolePrefabs.GameObject(), position, Quaternion.identity);
         SpawnMsg[0].SpawnMessage();
+        //Destroys the object after 3 seconds
         Destroy(BlackHolePrefabs.GameObject(), 3f);        
     }
     
     public void Asteroids()
     {
+        //Quad and meshcollider to check the size of the screen, which allows us to find a vector2 to spawn the blackhole on the screen
+        mesh = quad.GetComponent<MeshCollider>();
         screenY = Random.Range(mesh.bounds.min.y, mesh.bounds.max.y);
         position = new Vector2(3.5f, screenY);
 
+        //Spawns the object on the position with the spawnmsg on console
         Instantiate(AsteroidPrefabs.GameObject(), position, Quaternion.identity);
         SpawnMsg[1].SpawnMessage();
+        //Destroys the object after 10 seconds
         Destroy(AsteroidPrefabs.GameObject(), 10f);        
     }
 
     public void Satellite()
     {
+        //Quad and meshcollider to check the size of the screen, which allows us to find a vector2 to spawn the blackhole on the screen
+        mesh = quad.GetComponent<MeshCollider>();
         screenY = Random.Range(mesh.bounds.min.y, mesh.bounds.max.y);
         position = new Vector2(3.5f, screenY);
 
+        //Spawns the object on the position with the spawnmsg on console
         Instantiate(SatellitePrefabs.GameObject(), position, Quaternion.identity);
         SpawnMsg[2].SpawnMessage();
+        //Destroys the object after 10 seconds
         Destroy(SatellitePrefabs.GameObject(), 10f);
     }
 }
